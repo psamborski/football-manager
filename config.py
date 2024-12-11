3f9e39fc62eb7f7475ced23b6a6ea53560be9b26
psamborski <psambek@gmail.com>
2024-12-12 00:38:48 +0100

    Env-dependent DB. Reorganised DB files. Added a lot of new data to DB.
    DB data:
    - 9k new players
    - top 5 leagues + polish leagues
    - clubs belonging to mentioned leagues
    - players nationalities

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
+# 6. Warstwa Konfiguracji (Configuration Layer)
 import os
 
+# Get application stage from environment
+app_stage = os.getenv('STAGE')
+
+# Base directory
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'database', 'fm.db')}"
+
+# Database directory
+DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'app', 'database', 'fm.db' if app_stage == 'PRODUCTION' else 'fm_dev.db')}"
