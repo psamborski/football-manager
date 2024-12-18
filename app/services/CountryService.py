656f774bde4717f17b198bd34b7bf8180c7b7677
psamborski <psambek@gmail.com>
2024-12-18 23:56:56 +0100

    Add unstaged changes to previous commit

--- app/services/CountryService.py
+++ app/services/CountryService.py
@@ -7,10 +7,24 @@ from app.utils import reformat_country_data_from_db
 
 
 class CountryService:
+    """
+    A service class that handles operations related to countries.
+    Provides methods to interact with the database through resources and return formatted country data.
+    """
+
     @staticmethod
     def get_all_countries():
+        """
+        Retrieve all available countries from the database, sorted in descending order by name.
+        The retrieved data is reformatted and represented as a list of CountryModel objects.
+
+        Returns:
+            list[CountryModel]: A list of country models containing the reformatted country data.
+        """
+        # Access the database session using the context manager
         with get_db_session() as db_session:
             country_resource = CountryResource(db_session)
+
             raw_data = country_resource.get_all_countries(order_by=CountrySchema.name.desc())
 
             return [
--- app/services/PlayerService.py
+++ app/services/PlayerService.py
@@ -1,15 +1,34 @@
-from app.resources.PlayerResource import PlayerResource
+from app.database import get_db_session, PlayerSchema
 from app.models.PlayerModel import PlayerModel
+from app.resources.PlayerResource import PlayerResource
 from app.utils import reformat_player_data_from_db
 
 
 class PlayerService:
+    """ PlayerService provides functionality to fetch and process player-related data. """
+
     @staticmethod
     def get_top_players_from_country(country_id, limit=100):
-        raw_players = PlayerResource.get_players_by_country(country_id, limit, order_by="skill_rating DESC")
+        """
+        Fetch and process the top players from a specific country based on skill rating.
 
-        return [
-            PlayerModel(
-                **reformat_player_data_from_db(player)
-            ) for player in raw_players
-        ]
+        Args:
+            country_id (int): The ID of the country for which players will be fetched.
+            limit (int, optional): The maximum number of players to retrieve. Defaults to 100.
+
+        Returns:
+            List[PlayerModel]: A list of PlayerModel objects representing the top players
+            from the specified country, ordered by skill rating in descending order.
+        """
+        with get_db_session() as db_session:
+            player_resource = PlayerResource(db_session)
+
+            raw_players = player_resource.get_players_by_country(
+                country_id, limit, order_by=PlayerSchema.skill_rating.desc()
+            )
+
+            return [
+                PlayerModel(
+                    **reformat_player_data_from_db(player)
+                ) for player in raw_players
+            ]
